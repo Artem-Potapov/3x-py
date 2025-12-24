827b7768ff8daa4af46e1d8da2a7091ac658ec29
Artem-Potapov <potapov2350@gmail.com>
2025-12-24 04:57:39 +0300

    add server endpoints

--- endpoints.py
+++ endpoints.py
@@ -1,24 +1,72 @@
-from typing import Generic, Type
+from typing import Generic, Type, Literal, LiteralString
 
+from httpx import Response
 from pydantic.main import ModelT
 
+import models
 from api import XUIClient
 from util import camel_to_snake
-from pydantic_models import Inbound
+from models import Inbound
 
 
 class BaseEndpoint(Generic[ModelT]):
-    _model: Type[ModelT]
     _url: str
 
     def __init__(self, client: "XUIClient") -> None:
         self.client = client
 
+class Server(BaseEndpoint):
+    _url = "/panel/api/server"
+
+    async def _get_simple_response(self, caller_endpoint: str):
+        resp = await self.client.safe_get(f"{self._url}{caller_endpoint}")
+        if resp.status_code == 200:
+            resp_json = resp.json()["obj"]
+            return resp_json
+        elif resp.status_code == 518:
+            raise RuntimeError("Error: too many retries")
+        else:
+            raise RuntimeError(f"Error: wrong status code {resp.status_code}")
+
+
+    async def new_uuid(self) -> str:
+        endpoint = "/getNewUUID"
+        resp_json = await self._get_simple_response(endpoint)
+        return resp_json["uuid"]
+
+    async def new_x25519(self) -> dict[Literal["privateKey", "publicKey"], str]:
+        endpoint = "/getNewX25519Cert"
+        resp_json = await self._get_simple_response(endpoint)
+        return resp_json
+
+    async def new_mldsa65(self) -> dict[Literal["verify", "seed"], str]:
+        endpoint = "/getNewmldsa65"
+        resp_json = await self._get_simple_response(endpoint)
+        return resp_json
+
+    async def new_mlkem768(self) -> dict[Literal["client", "seed"], str]:
+        endpoint = "/getNewmlkem768x"
+        resp_json = await self._get_simple_response(endpoint)
+        return resp_json
+
+
 
 
 class Inbounds(BaseEndpoint):
-    _model = Inbound
     _url = "/panel/api/inbounds"
 
-    async def get(self):
+    async def get_all(self):
         endpoint = "/list"
+
+    async def get_specific(self, id):
+        endpoint = f"/get/{id}"
+
+    async def get_client_with_email(self, email):
+        endpoint = f"/getClientTraffics/{email}"
+
+    async def get_client_with_uuid(self, uuid):
+        endpoint = f"/getClientTrafficsById/{uuid}"
+
+    async def add_client(self, client: models.InboundClients|models.SingleInboundClient):
+        endpoint = f"/inbounds/addClient/"
+
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
+import asyncio
 import logging
+import random
 import re
-from typing import TypeAlias, Union, Dict, Any, List
+from typing import TypeAlias, Union, Dict, Any, List, overload
 import httpx
 
 JsonType: TypeAlias = Union[Dict[Any, Any], List[Any]]
@@ -9,24 +11,51 @@ JsonType: TypeAlias = Union[Dict[Any, Any], List[Any]]
 _RE_CAMEL_TO_SNAKE1 = re.compile("(.)([A-Z][a-z]+)")
 _RE_CAMEL_TO_SNAKE2 = re.compile("([a-z0-9])([A-Z])")
 
-
 def camel_to_snake(name: str) -> str:
     name = re.sub(_RE_CAMEL_TO_SNAKE1, r"\1_\2", name)
     return re.sub(_RE_CAMEL_TO_SNAKE2, r"\1_\2", name).lower()
 
-async def check_xui_response_validity(response: JsonType) -> str:
-    json_resp = response
+
+async def async_range(start, stop=None, step=1):
+    if stop:
+        range_ = range(start, stop, step)
+    else:
+        range_ = range(start)
+    for i in range_:
+        yield i
+        await asyncio.sleep(0)
+
+
+def generate_random_email(length: int = 8) -> str:
+    s = ""
+    for i in range(length):
+        s += random.choice("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return s
+
+
+def generate_new_subscription(length: int = 16):
+    s = ""
+    for i in range(length):
+        s += random.choice("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return s
+
+
+async def check_xui_response_validity(response: JsonType | httpx.Response) -> str:
+    if isinstance(response, httpx.Response):
+        json_resp = response.json()
+    else:
+        json_resp = response
+
     if len(json_resp) == 3:
         if tuple(json_resp.keys()) == ("success", "msg", "obj"):
             success: bool = json_resp["success"]
             msg: str = json_resp["msg"]
-            obj: JsonType = json_resp["obj"]
             if success:
                 return "OK"
             if "database" in msg.lower() and "locked" in msg.lower() and not success:
                 logging.log(logging.WARNING, "Database is locked, retrying...")
                 return "DB_LOCKED"
-    return "ERROR"
+    raise RuntimeError("Validator got something very unexpected (Please don't shove responses with non-20X status codes in here...)")
 
 class DBLockedError(Exception):
     def __init__(self, message):
